617a459e611709bd684f2ddb3201fc4bd5c91e56
cnmmp01 <e4problem@gmail.com>
2015-07-21 16:45:37 +0800

    upload projects

---
+++ web4stone/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(web4stone.Startup))]
+namespace web4stone
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
